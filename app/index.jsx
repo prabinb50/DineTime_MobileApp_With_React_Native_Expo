a09e0068a8839ffaaba8efc0b2ce117ce1d300a4
Prabin Joshi <joshiprabin17@gmail.com>
2025-05-05 23:43:39 +0545

    updated

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className=""
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-purple-600 text-xl">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
