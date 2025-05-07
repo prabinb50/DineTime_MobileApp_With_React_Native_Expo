ea90f5ab12e2c9ccbab8856d6129998b6793358c
Prabin Joshi <joshiprabin17@gmail.com>
2025-05-07 15:33:05 +0545

    Add SignIn and SignUp screens with validation; include Formik and Yup for form handling

---
+++ app/(auth)/signin.jsx
@@ -0,0 +1,84 @@
+import { Image, ScrollView, StatusBar, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
+import logo from "../../assets/images/dinetimelogo.png"
+import emptyImg from "../../assets/images/Frame.png"
+import { Formik } from "formik";
+import validationSchema from "../../utils/authSchema";
+
+const SignIn = () => {
+    const router = useRouter();
+
+    const handleSignIn = () => { };
+
+    return (
+        <SafeAreaView className="bg-[#2b2b2b]">
+            <StatusBar barStyle={"light-content"} />
+            <ScrollView contentContainerStyle={{ height: "100%" }}>
+                <View className="m-2 flex justify-center items-center">
+                    <Image
+                        source={logo}
+                        style={{ width: 200, height: 100 }}
+                    />
+
+                    <Text className="text-lg text-center text-white font-bold mb-5">Let's get you started</Text>
+
+                    <View className="w-5/6">
+                        <Formik
+                            initialValues={{ email: "", password: "" }}
+                            validationSchema={validationSchema}
+                            onSubmit={handleSignIn}>
+                            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                                <View className="w-full">
+                                    <Text className="text-[#f49b33] mt-4 mb-2">Email</Text>
+                                    <TextInput
+                                        className="h-10 border border-white rounded px-2 text-white"
+                                        keyboardType="email-address"
+                                        onChangeText={handleChange("email")}
+                                        value={values.email}
+                                        onBlur={handleBlur("email")}
+                                    />
+                                    {errors.email && touched.email && (<Text className="text-red-500 text-xs mb-2">{errors.email}</Text>)}
+
+                                    <Text className="text-[#f49b33] mt-4 mb-2">Password</Text>
+                                    <TextInput
+                                        className="h-10 border border-white rounded px-2 text-white"
+                                        secureTextEntry
+                                        onChangeText={handleChange("password")}
+                                        value={values.password}
+                                        onBlur={handleBlur("password")}
+                                    />
+                                    {errors.password && touched.password && (<Text className="text-red-500 text-xs mb-2">{errors.password}</Text>)}
+
+                                    <TouchableOpacity
+                                        className="p-2 bg-[#f49b33] text-black rounded-lg mt-6"
+                                        onPress={handleSubmit}>
+                                        <Text className="text-lg font-semibold text-center">Sign In</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            )}
+                        </Formik>
+
+                        <View >
+                            <TouchableOpacity onPress={() => router.push("/signup")} className="flex flex-row justify-center items-center my-5 p-2">
+                                <Text className="text-white font-semibold">Don't have an account? {" "}</Text>
+
+                                <Text className="text-base font-semibold underline text-[#f49b33]">Sign Up</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+
+                <View className="flex-1">
+                    <Image
+                        source={emptyImg}
+                        className="w-full h-full"
+                        resizeMode="contain"
+                    />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default SignIn
---
+++ utils/authSchema.js
@@ -0,0 +1,17 @@
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string()
+        .email("Invalid email")
+        .required("Email is required"),
+    password: Yup.string()
+        .min(6, "Password must be at least 6 characters")
+        .matches(/[a-zA-Z]/, "Password can only contain letters")
+        .matches(/[0-9]/, "Password must contain at least one number")
+        .matches(/[@$!%*?&]/, "Password must contain at least one special character")
+        .matches(/^(?=.*[A-Z])/, "Password must contain at least one uppercase letter")
+        .matches(/^(?=.*[a-z])/, "Password must contain at least one lowercase letter")
+        .required("Password is required"),
+});
+
+export default validationSchema;
