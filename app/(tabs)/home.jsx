012c8eeb3de8d45c8702388893638eda6cf53fb9
Prabin Joshi <joshiprabin17@gmail.com>
2025-05-10 17:35:16 +0545

    Refactor Home component to include a logo and welcome message within a styled SafeAreaView

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, Platform } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import logo from "../../assets/images/dinetimelogo.png"
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView className="bg-[#2b2b2b] ">
+            <View className="flex items-center">
+                <View className="bg-[#5f5f5f] w-11/12 rounded-lg shadow-lg justify-between items-center flex-row p-2">
+                    <View className="flex flex-row">
+                        <Text
+                            className="text-base h-10 align-middle text-white"
+                            style={{ paddingTop: Platform.OS === "android" ? 6.5 : 8 }}>{" "}
+                            Welcome to{" "}</Text>
+
+                        <Image
+                            resizeMode='cover'
+                            className="w-20 h-12"
+                            source={logo}
+                        />
+                    </View>
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
 
 export default Home
+
+// 2:07:21
